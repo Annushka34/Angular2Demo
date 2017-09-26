a4782e74529aafa89e6718f6caa60bac74105e1b
Anna Pilaeva <annsamoluk82@gmail.com>
2017-09-26 20:24:22 +0300

    classWork

--- Angular2Demo/src/app/app.component.js
+++ Angular2Demo/src/app/app.component.js
@@ -5,18 +5,50 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
-var AppComponent = (function () {
+var AppComponent = /** @class */ (function () {
     function AppComponent() {
-        this.name = 'Angular 2';
     }
+    //pageHeader: string ='Employee details';
+    //imagePath: string = '/images/dog1.jpg';
+    //firstName: string = 'Peter';
+    //lastName: string = 'Pen';
+    //isDisabled: boolean = true;
+    //badHtml: string = 'Hello <sript>alert("Hacked");</script>';
+    //getFullName(): string{
+    //    return this.firstName + ' ' + this.lastName;
+    //classesToApply: string = 'italicsClass boldClass';
+    //applyBoldClass: boolean = true;
+    //applyItalicsClass: boolean = true;
+    //addClasses()
+    //{
+    //    let classes={
+    //        boldClass: this.applyBoldClass,
+    //        italicsClass: this.applyItalicsClass
+    //    };
+    //    return classes;
+    //}
+    //isBold: boolean = true;
+    //fontSize: number = 30;
+    //isItalic: boolean = true;
+    //addStyles() {
+    //    let styles = {
+    //        'font-weight': this.isBold ? 'bold' : 'normal',
+    //        'font-style': this.isItalic ? 'italic':'normal'
+    //        };
+    //    return styles;
+    //}
+    AppComponent.prototype.onClickMe = function () {
+        console.log('Button Clicked');
+    };
+    AppComponent = __decorate([
+        core_1.Component({
+            selector: 'my-app',
+            templateUrl: "app/app.component.html"
+        })
+    ], AppComponent);
     return AppComponent;
 }());
-AppComponent = __decorate([
-    core_1.Component({
-        selector: 'my-app',
-        template: "<h1>Hello {{name}}</h1>",
-    })
-], AppComponent);
 exports.AppComponent = AppComponent;
 //# sourceMappingURL=app.component.js.map
--- Angular2Demo/src/app/app.component.ts
+++ Angular2Demo/src/app/app.component.ts
@@ -2,6 +2,46 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: `app/app.component.html`
 })
-export class AppComponent  { name = 'Angular 2'; }
+export class AppComponent  {
+    //pageHeader: string ='Employee details';
+    //imagePath: string = '/images/dog1.jpg';
+    //firstName: string = 'Peter';
+    //lastName: string = 'Pen';
+    //isDisabled: boolean = true;
+    //badHtml: string = 'Hello <sript>alert("Hacked");</script>';
+    //getFullName(): string{
+    //    return this.firstName + ' ' + this.lastName;
+
+    //classesToApply: string = 'italicsClass boldClass';
+    //applyBoldClass: boolean = true;
+    //applyItalicsClass: boolean = true;
+
+    //addClasses()
+    //{
+    //    let classes={
+    //        boldClass: this.applyBoldClass,
+    //        italicsClass: this.applyItalicsClass
+    //    };
+    //    return classes;
+    //}
+
+    //isBold: boolean = true;
+    //fontSize: number = 30;
+    //isItalic: boolean = true;
+
+    //addStyles() {
+    //    let styles = {
+    //        'font-weight': this.isBold ? 'bold' : 'normal',
+    //        'font-style': this.isItalic ? 'italic':'normal'
+    
+    //        };
+    //    return styles;
+    //}
+
+    onClickMe(): void {
+        console.log('Button Clicked');
+    }
+}
+
